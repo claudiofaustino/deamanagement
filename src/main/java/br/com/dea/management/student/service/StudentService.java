0fa44c4437fa91662b3371c1bd8bfbc44508ebc8
Claudio Faustino <claudio@MacBook-Pro-de-Claudio.local>
2023-02-01 07:37:10 -0300

    Semana 4 - first commit

--- src/main/java/br/com/dea/management/student/service/StudentService.java
+++ src/main/java/br/com/dea/management/student/service/StudentService.java
@@ -1,10 +1,12 @@
 package br.com.dea.management.student.service;
 
+import br.com.dea.management.exceptions.NotFoundException;
 import br.com.dea.management.student.domain.Student;
 import br.com.dea.management.student.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +22,11 @@ public class StudentService {
     }
 
     public Page<Student> findAllStudentsPaginated(Integer page, Integer pageSize) {
-        return this.studentRepository.findAllPaginated(PageRequest.of(page, pageSize));
+        return this.studentRepository.findAllPaginated(PageRequest.of(page, pageSize, Sort.by("user.name").ascending()));
+    }
+
+    public Student findStudentById(Long id) {
+        return this.studentRepository.findById(id).orElseThrow(() -> new NotFoundException(Student.class, id));
     }
 
 }
